c590aff135d3a46cb73ee6bbde900ebb60229b62
akash8992 <akash8992@gmail.com>
2024-09-26 18:10:13 +0530

    terraform-docker

--- terraform-local/local.tf
+++ terraform-local/local.tf
@@ -1,4 +1,15 @@
 resource "local_file" "devops" {
-        filename = "/home/ubuntu/terraform-devops/terraform-local/devops_automated.txt"
+        filename = "/Users/apple/Desktop/devops-study/terraform-devops/terraform-local/devops_automated.txt"
         content = "i want to become a devops enginner"
+}
+
+
+resource "random_string" "rand-str" { 
+    length = 16 
+    special = true 
+    override_special = "!#$%&*()-_=+[]{}<>:?" 
+}
+output "random_string" {
+value = random_string.rand-str[*].result
+
 }
